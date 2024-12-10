a7537eee43938be88f405280f900423c2eae3dda
Ibinthomas <ibinthomas858@gmail.com>
2024-12-10 17:29:16 +0530

    edupro

---
+++ edupro1/app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.index),
+    # path('',views.about),
+    path('contact',views.contact),
+    path('courses',views.courses),
+    path('placement',views.placement),
+    path('about',views.about),
+    
+    
+    
+]
